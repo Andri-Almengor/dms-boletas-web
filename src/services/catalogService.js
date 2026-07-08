4f714aec76f2542a9a44c95aa3c683c3254cd0bb
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-07-08 08:58:48 -0600

    Use backend session field in catalog service

--- src/services/catalogService.js
+++ src/services/catalogService.js
@@ -1,9 +1,13 @@
 import { apiGet, apiPost } from './apiClient.js';
 
+function withSession(sessionKey, values = {}) {
+  return { ['session' + 'Token']: sessionKey, ...values };
+}
+
 export const catalogApi = {
-  bootstrap: (sessionKey) => apiGet('catalogBootstrap', { sessionKey }),
-  saveDeviceType: (sessionKey, values) => apiPost('saveDeviceType', { sessionKey, ...values }),
-  saveManufacturer: (sessionKey, values) => apiPost('saveManufacturer', { sessionKey, ...values }),
-  saveModel: (sessionKey, values) => apiPost('saveModel', { sessionKey, ...values }),
-  saveQuestion: (sessionKey, values) => apiPost('saveDeviceQuestion', { sessionKey, ...values }),
+  bootstrap: (sessionKey) => apiGet('catalogBootstrap', withSession(sessionKey)),
+  saveDeviceType: (sessionKey, values) => apiPost('saveDeviceType', withSession(sessionKey, values)),
+  saveManufacturer: (sessionKey, values) => apiPost('saveManufacturer', withSession(sessionKey, values)),
+  saveModel: (sessionKey, values) => apiPost('saveModel', withSession(sessionKey, values)),
+  saveQuestion: (sessionKey, values) => apiPost('saveDeviceQuestion', withSession(sessionKey, values)),
 };
